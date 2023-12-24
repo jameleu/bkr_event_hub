ff03884e3499b11b5bec24faa746ebc151d2f898
James Leung <jleung2101@gmail.com>
2023-12-23 19:17:19 -0800

    updated event and user api endpoints to be queriable by user for rud so accessible

--- form_be/event/views.py
+++ form_be/event/views.py
@@ -1,7 +1,9 @@
 # events/views.py
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Event, Attendance, Waitlist
 from .serializers import EventSerializer, AttendanceSerializer, WaitlistSerializer
+from django.shortcuts import get_object_or_404
 
 class EventListAPIView(generics.ListCreateAPIView):
     queryset = Event.objects.all()
@@ -15,9 +17,62 @@ class AttendanceListAPIView(generics.ListCreateAPIView):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
 
+# class AttendanceListAPIView(generics.ListCreateAPIView):
+#     serializer_class = AttendanceSerializer
+
+#     def get_queryset(self):
+#         email = self.request.query_params.get('email', None)
+        
+#         if email:
+#             # If email parameter is provided, filter attendance based on the user's email
+#             user_attendance = Attendance.objects.filter(user__student_email=email)
+#             return user_attendance
+#         else:
+#             # If no email parameter is provided, return the default queryset (all attendance)
+#             return Attendance.objects.all()
+
+#     def delete(self, request, *args, **kwargs):
+#         email = self.request.query_params.get('email', None)
+        
+#         if email:
+#             # If email parameter is provided, delete attendance records based on the user's email
+#             user_attendance = Attendance.objects.filter(user__student_email=email)
+            
+#             if user_attendance.exists():
+#                 user_attendance.delete()
+#                 return Response({'detail': f'Attendance records for {email} deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+#             else:
+#                 return Response({'detail': f'No attendance records found for {email}'}, status=status.HTTP_404_NOT_FOUND)
+#         else:
+#             return Response({'detail': 'Email parameter is required for deletion'}, status=status.HTTP_400_BAD_REQUEST)
+
 class AttendanceDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
+    lookup_field = 'user__student_email'  # Assuming 'user' is a ForeignKey in the Attendance model
+
+    def get_object(self):
+        email = self.kwargs.get('email')
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset, user__student_email=email)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        email = self.kwargs.get('email')
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        email = self.kwargs.get('email')
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class WaitlistListAPIView(generics.ListCreateAPIView):
     queryset = Waitlist.objects.all()
@@ -26,3 +81,26 @@ class WaitlistListAPIView(generics.ListCreateAPIView):
 class WaitlistDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Waitlist.objects.all()
     serializer_class = WaitlistSerializer
+    lookup_field = 'user__student_email'  # Assuming 'user' is a ForeignKey in the Waitlist model
+
+    def get_object(self):
+        email = self.kwargs.get('email')
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = get_object_or_404(queryset, user__student_email=email)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+    def update(self, request, *args, **kwargs):
+        email = self.kwargs.get('email')
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        email = self.kwargs.get('email')
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- form_be/user/views.py
+++ form_be/user/views.py
@@ -1,20 +1,67 @@
 # users/views.py
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import User, Admin
 from .serializers import UserSerializer, AdminSerializer
 
 class UserListAPIView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
+    
 class UserDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    lookup_field = 'email'  # Use the field by which you want to look up the user
+
+    def get_object(self):
+        email = self.kwargs.get('email')
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise generics.Http404('User not found')
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
 class AdminListAPIView(generics.ListCreateAPIView):
     queryset = Admin.objects.all()
     serializer_class = AdminSerializer
 
 class AdminDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Admin.objects.all()
     serializer_class = AdminSerializer
+    def get_object(self):
+        email = self.kwargs.get('email')
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise generics.Http404('Admin not found')
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
