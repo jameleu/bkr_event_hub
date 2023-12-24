84a9ffba6b5aa909a5960ff44ee62c533143d89d
James Leung <jleung2101@gmail.com>
2023-12-23 18:44:41 -0800

    fixed typos so urls are correct in membership app

--- form_be/membership/urls.py
+++ form_be/membership/urls.py
@@ -1,12 +1,11 @@
 # urls.py
 from django.urls import path
 from .views import BlacklistCheckAPIView, MembershipDetailAPIView,\
-    MembershipListAPIView, BlacklistDetailAPIView, BlacklistStatusAPIView
+    MembershipListAPIView, BlacklistDetailAPIView
 
 urlpatterns = [
-    path('api/blacklist/check/<str:username>/', BlacklistCheckAPIView.as_view(), name='blacklist-check'),
+    path('api/blacklist/<str:username>/', BlacklistDetailAPIView.as_view(), name='blacklist-detail'),
     path('api/memberships/<int:pk>/', BlacklistDetailAPIView.as_view(), name='membership-detail'),
-    path('api/blacklist/status/<str:username>/', BlacklistStatusAPIView.as_view(), name='blacklist-status'),
     path('api/memberships/', MembershipListAPIView.as_view(), name='membership-list'),
-    path('api/memberships/<int:pk>/', MembershipDetailAPIView.as_view(), name='membership-detail')
+    path('api/memberships/<str:username>/', MembershipDetailAPIView.as_view(), name='membership-detail')
 ]
