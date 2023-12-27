493fb71f318683d1188c08a5b340d71403f04326
James Leung <jleung2101@gmail.com>
2023-12-26 22:23:30 -0800

    added models to admin so could modify as admin

--- form_be/event/admin.py
+++ form_be/event/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Event, Waitlist, Attendance
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Waitlist)
+admin.site.register(Attendance)
